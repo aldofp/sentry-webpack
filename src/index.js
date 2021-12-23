630756b4e7fe518624a58637d55a61460efa71ac
aldofp <aldo.putra@medicaboo.com>
2021-12-23 16:39:32 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: "https://696fc64169bf401f8ba90f8c9a3b8dd9@o1097566.ingest.sentry.io/6119189",
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
     <App />,
