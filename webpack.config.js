c9c022c295b688e68d7487d91536dd9c28ab5c62
aldofp <aldo.putra@medicaboo.com>
2021-12-24 09:02:51 +0700

    first commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const SentryWebpackPlugin = require("@sentry/webpack-plugin");
 
 module.exports = {
+  devtool: 'source-map',
   entry: path.join(__dirname, "src", "index.js"),
   mode: 'production',
   output: {
@@ -24,6 +26,18 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: path.join(__dirname, "src", "index.html"),
+    }),
+    new SentryWebpackPlugin({
+      // sentry-cli configuration - can also be done directly through sentry-cli
+      // see https://docs.sentry.io/product/cli/configuration/ for details
+      authToken: "adbf2233a4b541b5ad96010879b05c6ef6265d52ee3b4444968953aad90d7a6f",
+      org: "aldo-jy",
+      project: "react",
+      release: "1.0.0",
+
+      // other SentryWebpackPlugin configuration
+      include: "dist",
+      ignore: ["node_modules", "webpack.config.js"],
     }),
   ],
 }
